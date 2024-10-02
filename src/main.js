3c845e1938799b03b5c7a76c32e9e0b6eb35beb4
Mete <metlandus@hotmail.com.tr>
2024-10-02 20:09:26 +0300

    Func and styles are done

--- src/main.js
+++ src/main.js
@@ -16,16 +16,21 @@ form.addEventListener('submit', async (event) => {
     gallery.innerHTML = '';
 
     const apiKey = '19490895-43525fbfea01be26fe968a218';
-    const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=photo&per_page=12`;
+    const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
 
     try {
+        gallery.innerHTML = '<div class="loader"></div>';
         const response = await fetch(apiUrl);
         const data = await response.json();
 
+
         if (data.hits.length > 0) {
+            gallery.innerHTML = '';
             displayImages(data.hits);
 
-            new SimpleLightbox('.gallery a');
+            const lightbox = new SimpleLightbox('#gallery a');
+            lightbox.refresh();
+
         } else {
             iziToast.error({
                 title: 'No Results',
@@ -42,20 +47,32 @@ form.addEventListener('submit', async (event) => {
     }
 });
 
+
 function displayImages(images) {
-    images.forEach(image => {
-        const aElement = document.createElement('a');
-        aElement.href = image.largeImageURL;
-        aElement.classList.add('gallery-item');
-        aElement.setAttribute('data-lightbox', 'gallery');
-
-        const imgElement = document.createElement('img');
-        imgElement.src = image.webformatURL;
-        imgElement.alt = image.tags;
-        imgElement.classList.add('gallery-image');
-
-        aElement.appendChild(imgElement);
-
-        gallery.appendChild(aElement);
+    return images.forEach(image => {
+        gallery.innerHTML += `
+            <li class="gallery-item">
+            <a class="gallery-link" href="${image.largeImageURL}">
+                <img class="gallery-image" src="${image.largeImageURL}" alt="${image.tags}" title="${image.tags}">
+            </a>
+                <div class="image-captions">
+                <div class="image-caption-datas">
+                <p class="img-caption-head">Views</p>
+                <p class="img-caption-value">${image.views}</p>
+                </div>
+                <div class="image-caption-datas">
+                <p class="img-caption-head">Downloads</p>
+                <p class="img-caption-value">${image.downloads}</p>
+                </div>
+                <div class="image-caption-datas">
+                <p class="img-caption-head">Likes</p>
+                <p class="img-caption-value">${image.likes}</p>
+                </div>
+                <div class="image-caption-datas">
+                <p class="img-caption-head">Comments</p>
+                <p class="img-caption-value">${image.comments}</p>
+                </div>
+            </li>
+        `;
     });
 }
